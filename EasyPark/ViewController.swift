d7a500a1e5daec94bbb590fc878894a62bbdb967
INFTEL 13 <inftel13@imac-de-master-inftel-50.local>
2016-01-22 09:54:24 +0100

    Initial Commit

---
+++ EasyPark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyPark
+//
+//  Created by INFTEL 13 on 22/1/16.
+//  Copyright © 2016 INFTEL 13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
